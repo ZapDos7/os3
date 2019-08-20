42251d30e33e036170496bbcb3786c611867c3f3
ZapDos7 <ioanna.zapalidi@gmail.com>
2019-08-20 16:36:06 +0300

    fixed fork

--- main.c
+++ main.c
@@ -26,6 +26,7 @@ int main (int argc, char*argv[]) {
     }
     else if (argc==1) {
         pnum = 5; //default value
+        fptr = fopen("randomfile.txt", "r");
     }
     else {
         fprintf(stderr, "How did you do that?\n");
@@ -52,68 +53,33 @@ int main (int argc, char*argv[]) {
 /////////////////////counters////////////////////////////
     int ppcount=0;//posa tupwthikan apo to idio P
 //////////////////////fork time//////////////////////////
-    int pids[pnum+2]; //table of pids returned by fork, 1 is C, the rest are P, + 1 for parent
-    for (int i = 0; i < pnum+2; i++) {
-        pids[i] = fork();    //fork
-        if (pids[i] == -1) {
+    int pid;
+    for (int i = 0; i < pnum+1; i++) {
+        pid = fork();//fork
+        if (pid == -1) {
 			fprintf(stderr, "Error! Fork failure.\n");
 			exit(-1);
         }
-///////////////////parent////////////////////////////////
-        time_t t;
-        int status;
-        if (pids[i]==0) {
-            printf("My id is %d. I am parent.\n", getpid());
-            //handle children
-            for (i=0;i<pnum+2;i++) {
-                /*waitpid(pids[i]);
-                fprintf(stderr, "Target taken down.\n");*/
-                /*if ((pids[i] = waitpid(pids[i], &status, WNOHANG)) == -1)
-                    perror("wait() error");
-                else if (pids[i] == 0) {
-                    time(&t);
-                    printf("child is still running at %s", ctime(&t));
-                    sleep(1);
-                }
-                else {
-                    if (WIFEXITED(status)) {
-                        printf("child exited with status of %d\n", WEXITSTATUS(status));
-                    }
-                    else {
-                        puts("child did not exit successfully");
-                    }
-                }
-            }
-*/
-            
-            //kill children
-            for (i=0;i<pnum+2;i++) {
-                kill(pids[i], SIGKILL);
-                fprintf(stderr, "Target taken down.\n");
-            }
-        } 
-        //else if (pids[i]>0) {
 /////////////////////////C///////////////////////////////
-            if(i==0) { 
-                printf("My id is %d. I am child. I am C.\n", getpid());
-                //the C: read & hash
-                //send back to out-ds
-            }
-            else {
-/////////////////////////P///////////////////////////////
-                printf("My id is %d. I am child.I am P%d\n", getpid(), i);
-                //the N Ps: read file
-                msg * shm_ptr; //init shared memory of msg size
-                shm_ptr = shm_attach(shm_id); //attach to shared memory
-
-                //send struct into in-ds
-
-                //the N Ps: read if out-ds is readable
-                //print stuff and update counters
-                //if idio Ppid = Ppid: ppcounter++;
-            }
+        else if (pid==0) {
+            printf("My id is %d. I am parent.I am C.\n", getpid());
+            //handle children - wait
+            //the C: read & hash
+            //send back to out-ds
+            //kill children
         }
-        else {
+/////////////////////////P///////////////////////////////
+        else if (pid>0) {
+            printf("My id is %d. I am child.I am P%d\n", getpid(), i);
+            //the N Ps: read file
+            msg * shm_ptr; //init shared memory of msg size
+            shm_ptr = shm_attach(shm_id); //attach to shared memory
+            //send struct into in-ds
+            //read if out-ds is readable
+            //print stuff and update counters
+            //if idio Ppid = Ppid: ppcounter++;
+        }
+        else {//useless else but you can never know with these magic stuff!
             fprintf(stderr, "How and why\n");
             exit(-1);
         }
